d805b5bb8a6a5db6e641fad8a621171397e223fd
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-19 11:18:51 -0700

    Update CSS file versions for mobile and non-mobile devices (Bump)
    Updated the version numbers of the CSS files linked in the HTML
    document. For mobile devices, the CSS file version is updated
    from 1.0.3 to 1.0.5. For non-mobile devices, the CSS file
    version is updated from 1.0.3 to 1.0.4.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=1.0.3';
+    link.href = 'm.index.css?v=1.0.5';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'index.css?v=1.0.3';
+    link.href = 'index.css?v=1.0.4';
     head.appendChild(link);
 }
