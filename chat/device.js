9f3b22eda831729a3ac1af95fbd0343d7a5d3b9e
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-19 15:17:40 -0700

    Refactor stylesheet loading and add mobile-specific styles
    Refactored the logic for detecting device type and loading the
    appropriate stylesheet from `index.js` to a new `device.js` file.
    Introduced a new mobile-specific stylesheet `m.chat.css` to
    enhance the user experience on mobile devices. Updated `chat.css`
    to slightly reduce the max-height of the chat container. Modified
    `chat.html` and `index.html` to link to `device.js` instead of
    `chat.css` and `index.js` respectively.

---
+++ chat/device.js
@@ -0,0 +1,16 @@
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'm.chat.css?v=1.0.8';
+    head.appendChild(link);
+} else {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'chat.css?v=1.0.5';
+    head.appendChild(link);
+}
