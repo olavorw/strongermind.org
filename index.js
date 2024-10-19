bed3b4f99e2b1995542b5089d11b3f2d19939184
sahilcodes-max <choprasahil.sc@gmail.com>
2024-10-19 12:29:33 -0700

    Update scroller and mobile css.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=1.0.5';
+    link.href = 'm.index.css?v=1.0.6';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
