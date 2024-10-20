a8790bf7bbb1256ee89986c558428fee002f36cb
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-19 20:55:31 -0700

    Update stylesheets, remove old files, and add new themes
    Removed `chat-green.css` and `m.chat-green.css`. Updated stylesheet versions in `device.js` and `index.html`. Modified background and text colors in `index.css` and `m.index.css`. Added `chat-darkgreen.css` and `chat-darkteal.css` with new chat interface styles. Introduced `m.index-lightteal.css` with comprehensive web page styling, including a hero section, buttons, and responsive design.

--- device.js
+++ device.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=1.1.4';
+    link.href = 'm.index.css?v=1.1.5';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'index.css?v=1.1.0';
+    link.href = 'index.css?v=1.1.1';
     head.appendChild(link);
 }
