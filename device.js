17d5793feaebec4e7078edaa6c379c89e052db34
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-19 21:05:57 -0700

    Update CSS versions, colors, and device.js version
    Updated mobile and desktop CSS versions in `device.js` to `1.1.6` and `1.1.2` respectively. Changed background gradient, text color, button color, and `.delete-data-link` class color in `index.css` and `m.index.css` to new shades of green. Updated `device.js` version in `index.html` to `1.1.7`.

--- device.js
+++ device.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=1.1.5';
+    link.href = 'm.index.css?v=1.1.6';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'index.css?v=1.1.1';
+    link.href = 'index.css?v=1.1.2';
     head.appendChild(link);
 }
