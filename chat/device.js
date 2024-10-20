01d3c91a154f36cba6e691c5bc05f10ce39327b6
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-20 10:52:53 -0700

    Streamline script and stylesheet inclusion with versioning
    Updated `chat.html` and `index.html` to directly include `device.js` with version query parameters, ensuring consistent versioning and cache busting. Modified `device.js` to conditionally append the appropriate stylesheet for mobile or desktop users with specific version parameters, enhancing cache management and ensuring correct styles are applied based on device type.

--- chat/device.js
+++ chat/device.js
@@ -1,7 +1,16 @@
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-var head = document.getElementsByTagName('HEAD')[0];
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = (isMobile ? 'm.chat.css' : 'chat.css') + '?v=' + new Date().getTime();
-head.appendChild(link);
+if (isMobile) {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'm.chat.css?v=2.2.5';
+    head.appendChild(link);
+} else {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'chat.css?v=2.1.4';
+    head.appendChild(link);
+}
--- device.js
+++ device.js
@@ -1,7 +1,16 @@
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-var head = document.getElementsByTagName('HEAD')[0];
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = (isMobile ? 'm.index.css' : 'index.css') + '?v=' + new Date().getTime();
-head.appendChild(link);
+if (isMobile) {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'm.index.css?v=2.1.6';
+    head.appendChild(link);
+} else {
+    var head = document.getElementsByTagName('HEAD')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'index.css?v=2.1.2';
+    head.appendChild(link);
+}
