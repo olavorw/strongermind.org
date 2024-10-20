82146a11fd9aa7a9f6b60b6ceda1ce1cedd47d80
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-19 20:29:03 -0700

    Bump Versions
    Update resource versions for latest CSS and JS files
    
    Updated version numbers to ensure the latest versions are used:
    - `device.js`: `m.index.css` from `1.1.2` to `1.1.3`, `index.css` from `1.0.8` to `1.0.9`
    - `index.html`: `device.js` from `1.1.3` to `1.1.4`
    
    These updates help with cache busting and ensure users receive the most recent files, which may include bug fixes and new features.

--- device.js
+++ device.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=1.1.2';
+    link.href = 'm.index.css?v=1.1.3';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'index.css?v=1.0.8';
+    link.href = 'index.css?v=1.0.9';
     head.appendChild(link);
 }
