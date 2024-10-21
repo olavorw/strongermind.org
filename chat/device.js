bd8fcba2fff8dabb4f013cf0c61a242c6583d72f
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-20 18:32:33 -0700

    Bump Versions

--- chat/device.js
+++ chat/device.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.chat.css?v=3.0.5';
+    link.href = 'm.chat.css?v=3.0.6';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'chat.css?v=3.0.5';
+    link.href = 'chat.css?v=3.0.6';
     head.appendChild(link);
 }
