26fccdc139fece6596cc70787ba5a90a7cb9dfdd
Olav <73853649+olavorw@users.noreply.github.com>
2024-10-21 20:38:15 -0700

    Add new HTML file, update styles, and version updates
    * Added `challenges.html` with basic structure.
    * Updated CSS file versions in `device.js` and `index.html` to `v3.0.1`.
    * Enhanced `index.css`:
      * Increased `h1` font size and added text shadow.
      * Updated button styles: font size, text color, text shadow, and margin.
      * Added text shadow to `.text-content` and adjusted padding.
      * Added styles for fixed header and footer, including responsive design.
    * Updated `index.html`:
      * New header structure with logo and subtext.
      * Moved privacy policy and terms of service links to header.
      * Added footer with "© StrongerMind.org 2024".
    * Enhanced `m.index.css` with styles for fixed header and footer.

--- device.js
+++ device.js
@@ -4,13 +4,13 @@ if (isMobile) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'm.index.css?v=3.0.0';
+    link.href = 'm.index.css?v=3.0.1';
     head.appendChild(link);
 } else {
     var head = document.getElementsByTagName('HEAD')[0];
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'index.css?v=3.0.0';
+    link.href = 'index.css?v=3.0.1';
     head.appendChild(link);
 }
